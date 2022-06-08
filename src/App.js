0808aa6c951c1e026899f040170db493bea92342
Krishnan Shankar <krishnans2006@gmail.com>
2022-06-08 19:05:15 -0400

    Basic site structure, install + boilerplate redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+
+import Login from "./pages/Login.js";
+import Signup from "./pages/Signup.js";
+import Profile from "./pages/Profile.js";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <h1>camb.ai React project!</h1>
+            <Switch>
+                <Route exact path="/">
+                    <div>
+                        <h1>Home Page</h1>
+                        <a href="/Login.js">Login</a> or <a href="/Signup.js">Signup</a>
+                    </div>
+                </Route>
+
+                <Route path="/login">
+                    <Login />
+                </Route>
+
+                <Route path="/signup">
+                    <Signup />
+                </Route>
+
+                <Route path="/profile">
+                    <Profile />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/pages/Profile.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const Profile = () => {
+    return (
+        <div>
+            <h1>Profile</h1>
+        </div>
+    )
+}
+
+export default Profile;
